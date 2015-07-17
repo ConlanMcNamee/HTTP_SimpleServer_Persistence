7f3cc7d03dbc09e4cb132f9ca74c79c9579d7d17
Conlan McNamee <conlanmcnamee@gmail.com>
2015-07-16 19:34:52 -0700

    should have everything

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ function start() {
 		var fileName = ("./data" + path + ".json");
 		console.log(fileName);
 		if(req.method === "GET") {
+			if(req.url === "/") {
+				res.writeHead(404);
+				res.end("File not found");
+			}
 			fs.readFile(fileName, function(error, content) {
 				if(error) {
-					res.writeHead(500);
-					res.end()
+					res.writeHead(404);
+					res.end("<html><body><p>File not found</p></body></html>");
 				} else {
 					res.writeHead(200, {"Content-Type": 'application/json'});
 					res.end(content, "utf-8");
@@ -22,36 +26,73 @@ function start() {
 			});
 		} else if (req.method === "POST") {
 			console.log("post request received")
-			var body = " ";
+			//data is a parameter that req.on has
 			req.on('data', function(data) {
-				body += data.toString("utf-8");
-			});
-			console.log(body);
-			req.on('end', function() {
-
-
-			fs.writeFile(fileName, function(error, content) {
-				if(error) {
-					console.log("encountered error");
-				}
-					res.writeHead(200, {"Content-Type": "application/json"});
-					res.end("Success");
+				var body = data.toString();
+				console.log("on",body);
+				console.log("after",body);
+				req.on('end', function() {
+				fs.writeFile(fileName, body, function(error) {
+					if(error) {
+						console.log("encountered error");
+					}
+						res.writeHead(200, {"Content-Type": "application/json"});
+						res.end('{"Success": "true"}');
 
 				});
-			//res.end();
+			});
+			
+			
 		});
 
 		} else if (req.method === "PUT") {
+				console.log("put request received");
+				req.on('data', function(data) {
+					var body = data.toString('utf-8');
+					console.log(body);
+					var startObj = {
+						username: JSON.parse(body).username || null,
+						password: JSON.parse(body).password || null
+					}
+					console.log(startObj);
 
-		} else if (req.method === "PATCH") {
+					// body = JSON.parse(body);
+					fs.readdir('./data', function(err, files) {
+						if(err) return res.send(404, + "File not found");
+						console.log('data:', files);
+						for (var i = 0; i < files.length; i++) {
+							if("/" + files[i] === req.url + ".json") {
+								fs.readFile(fileName, function(err, data) {
+									var user = JSON.parse(data);
+									var obj = {username: null, password: null};
+									for (var prop in obj) {
+										obj[prop] = (startObj[prop]) ? startObj[prop] : user[prop];
+									}
+									fs.writeFile(fileName, JSON.stringify(obj), function(err) {
+										if(err) {
+										console.log("encountered error");
+										}
+										res.writeHead(200, {"Content-Type": "application/json"});
+										res.end('{"Success": "true"}');
 
+									});
+								});
+							} 
+						}
+					});
+			 	});
 		} else if (req.method === "DELETE") {
+			fs.unlink(fileName, function(err) {
+				if (err) {
+					res.writeHead(404, {"Content-Type": "text/html"});
+					res.end("<html><body><p>File not found</p></body></html>");
+				} else {
+					res.writeHead(200, {"Content-Type": "application/json"});
+					res.end('{"Deletion": "true"}');
+				}
+			});
 
 		}
-		// else {
-		// 	res.writeHead(404, {"Content-Type": "text/html"});
-		// 	res.end("<html><body><p> File not found</p></body></html>");
-		// }
 	}).listen(3000);
 	console.log("Server has started");
 }
